7f3ed9fe7c7b0c5271e827479ae3c384be9b5966
dix <163229876+dixfenix@users.noreply.github.com>
2024-04-09 16:13:33 +0500

    Update socialsData.js

--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -1,5 +1,5 @@
 export const socialsData = {
-    github: 'https://github.com/Ahrorjon91',
+    github: 'https://github.com/dixfenix',
     linkedIn: 'https://www.linkedin.com/in/ahrorjon91/',
     telegram: 'https://t.me/ahrorjon911',
     
