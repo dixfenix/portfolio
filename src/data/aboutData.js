236105ec835f4fc2ce91a2db241d0e44bd29bce8
dix <163229876+dixfenix@users.noreply.github.com>
2024-05-24 09:49:14 +0500

    Update aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "My name's Ahrorjon. I'm a DevOps engineer, System Administrator and Developer based in Khujand, TJK.",
-    description2: "A Systems Engineer is responsible for designing, implementing, and managing complex systems that integrate hardware, software, and network components. They ensure that all parts work together seamlessly to meet the needs of an organization. Their duties often include systems analysis, architecture design, integration, and troubleshooting, as well as ensuring system security and efficiency. Systems Engineers also focus on scalability, reliability, and maintainability of systems.",
+    description1: "My name's Ahrorjon. I'm a Systems engineer, Administrator and a part time Developer based in Khujand, TJK.",
+    description2: "As a Systems Engineer my responsiblities is designing, implementing, and managing complex systems that integrate hardware, software, and network components. I ensure that all parts work together seamlessly to meet the needs of an organization. My duties often include systems analysis, architecture design, integration, and troubleshooting, as well as ensuring system security and efficiency. We Engineers also focus on scalability, reliability, and maintainability of systems.",
     image: 2
 }
