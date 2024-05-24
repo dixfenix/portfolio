530239db2a80352c958618838c02a89fdc27bdf5
dix <163229876+dixfenix@users.noreply.github.com>
2024-05-24 09:42:14 +0500

    Update aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
     description1: "My name's Ahrorjon. I'm a DevOps engineer, System Administrator and Developer based in Khujand, TJK.",
-    description2: "During the day I work as a DevOps engineer at a HEXING group and in the evening I  work on freelance projects and utilize the time to built my own products. I spent my leisure hours writing articles and poetry. Right now I'm also trying a hand at machine learning and AI. I love to learn and explore new arenas.",
+    description2: "A Systems Engineer is responsible for designing, implementing, and managing complex systems that integrate hardware, software, and network components. They ensure that all parts work together seamlessly to meet the needs of an organization. Their duties often include systems analysis, architecture design, integration, and troubleshooting, as well as ensuring system security and efficiency. Systems Engineers also focus on scalability, reliability, and maintainability of systems.",
     image: 2
 }
